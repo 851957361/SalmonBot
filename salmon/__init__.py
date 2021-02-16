9509bff040df42309d3e852e4f196b311897abb2
しゃけてり <65025615+Watanabe-Asa@users.noreply.github.com>
2021-02-16 23:03:14 +0800

    Add files via upload

--- salmon/__init__.py
+++ salmon/__init__.py
@@ -1,1 +1,8 @@
+import nonebot
+from nonebot.adapters import Bot, Event, Message
+from .log import new_logger
 
+logger = new_logger('salmon')
+
+def getBot() -> Bot:
+    return list(nonebot.get_bots().values())[0]
