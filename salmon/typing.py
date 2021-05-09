d3ddf5c128e8ff7d1db72743b1133f5b77aa7357
WatanabeAsa <watanabe9940@gmail.com>
2021-05-10 01:19:59 +0800

    5/10

--- salmon/typing.py
+++ salmon/typing.py
@@ -1,7 +1,7 @@
 from typing import (Any, Callable, Dict, Iterable, List, NamedTuple, Optional,
                     Set, Tuple, Union, Type, Awaitable)
 from nonebot.typing import T_State, T_Handler, T_ArgsParser
-from nonebot.exception import FinishedException
+from nonebot.exception import FinishedException, ActionFailed
 from nonebot.adapters.cqhttp import Event as CQEvent
 from nonebot.adapters.cqhttp.message import MessageSegment, Message
-from nonebot.adapters.cqhttp.event import GroupMessageEvent, PrivateMessageEvent
+from nonebot.adapters.cqhttp.event import GroupMessageEvent, PrivateMessageEvent, MessageEvent
